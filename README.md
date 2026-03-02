534a8e46eeff8b5de3c5cc45c5d5ce4b86a1fbf6
Rafał -rsm- Marek <rsm@disroot.org>
2026-03-02 22:46:17 +0100

    Initial version on webserver and simple README

---
+++ README.md
@@ -0,0 +1,21 @@
+![alt text](resources/md-webserver-logo.png)
+
+# md-webserver
+md-webserver is a minimalist [Markdown](https://daringfireball.net/projects/markdown/)/[CommonMark](https://commonmark.org/) web server written in GNU Guile (Scheme).
+
+## Requirements
+- [GNU Guile](https://www.gnu.org/software/guile) >= 3.0
+- [(guile-commonmark)](https://github.com/OrangeShark/guile-commonmark) - for converting Markdown/CommonMark files to sxml format
+- [(htmlprag)](https://www.nongnu.org/guile-lib/doc/ref/htmlprag/) - for converting sxml data to html
+
+## Usage Examples
+### Minimal
+```shell
+guile -c "(use-modules (md-webserver)) (start-webserver '())"
+```
+
+### Typical
+(todo)
+
+## License
+GPLv3 or later. See LICENSE.
---
+++ main.scm
@@ -0,0 +1,121 @@
+
+(define-module (md-webserver main)
+  #:use-module (web uri)
+  #:use-module (web request)
+  #:use-module (web response)
+  #:use-module (web server)
+  #:use-module (commonmark sxml)
+  #:use-module (htmlprag)
+  #:use-module (ice-9 textual-ports)
+  #:use-module (ice-9 binary-ports)
+  #:use-module (ice-9 format)
+  #:export (ahref
+            start-webserver))
+
+
+(define (ahref url content)
+  `(a (@ (href ,url)) ,content))
+
+
+(define (templetize title lang charset extra-headers body-template body)
+  `(*TOP*
+    (*DECL* doctype html)
+    (html
+     ,(if lang `(@ (lang ,lang)) "")
+     (head
+      ,(if title `(title ,title) "")
+      ,(if charset `(meta (@ (charset ,charset))) "")
+      ,@(if extra-headers extra-headers '())
+      (meta (@ (name "generator") (content "md-webserver"))))
+     (body
+      ,@(if body-template (body-template body) body)))))
+
+
+(define default-conf
+  `((#:lang . "en")
+    (#:charset . "utf-8")
+    (#:content-dir . "./")
+    (#:index . "README.md")
+    ;; network stuff
+    (#:addr ,INADDR_LOOPBACK)
+    (#:port 8080)))
+
+
+(define (path-safe? path)
+  (cond ([null? path] #t)
+        ([string=? ".." (car path)] #f)
+        (else (path-safe? (cdr path)))))
+
+
+(define (file-ext fname)
+  (substring fname (1+ (string-rindex fname #\.))))
+
+
+(define (send-file fname mime-type)
+  (values
+   (build-response #:code 200
+                   #:headers `((content-type . ,mime-type)))
+   (call-with-input-file fname get-bytevector-all #:binary #t)))
+
+
+(define (gen-req-handler site-conf)
+  (λ (req req-body)
+    (catch #t
+      (λ ()
+        (let* ([path-oryg (split-and-decode-uri-path (uri-path (request-uri req)))]
+               [path-real (if [null? path-oryg]
+                              (list (assq-ref site-conf #:index))
+                              path-oryg)]
+               [full-path (string-append (assq-ref site-conf #:content-dir)
+                                         (string-join path-real "/"))])
+          (format #t "Serving file ~s [uri: ~s]~%" full-path path-real)
+          (cond ([and (path-safe? path-real) (file-exists? full-path)]
+                 (case [string->symbol (string-downcase (file-ext full-path))]
+                   ([md] (values
+                          (build-response #:code 200
+                                          #:headers '((content-type . (text/html))))
+                          (sxml->html
+                           (templetize
+                            (assq-ref site-conf #:title)
+                            (assq-ref site-conf #:lang)
+                            (assq-ref site-conf #:charset)
+                            (assq-ref site-conf #:extra-headers)
+                            (assq-ref site-conf #:body-template)
+                            (with-input-from-file full-path commonmark->sxml)))))
+                   ([css] (values
+                           (build-response #:code 200
+                                           #:headers '((content-type . (text/css))))
+                           (call-with-input-file full-path get-string-all)))
+                   ([jpg jpeg] (send-file full-path '(image/jpeg)))
+                   ([png] (send-file full-path '(image/png)))
+                   ([ico] (send-file full-path '(image/x-icon)))
+                   (else
+                    (format (current-error-port)
+                            "Warning! Unsupported file type: ~s~%" path-real)
+                    (values
+                     (build-response #:code 415
+                                     #:headers '((content-type . (text/plain))))
+                     (format #f "Unsupported Media Type: ~a"
+                             (file-ext (car (last-pair path-real))))))))
+                (else
+                 (format (current-error-port)
+                         "Warning! File not found: ~s~%" path-real)
+                 (values
+                  (build-response #:code 404
+                                  #:headers '((content-type . (text/plain))))
+                  "404 Not Found")))))
+      (λ (key . args)
+        (let ([err-desc (format #f "Error! ~a: ~a. ~?~%"
+                                key (car args) (cadr args) (caddr args))])
+          (display err-desc (current-error-port))
+          (values
+           (build-response #:code 500
+                           #:headers '((content-type . (text/plain))))
+           err-desc))))))
+
+
+(define (start-webserver site-conf)
+  (let* ([conf (append site-conf default-conf)]
+         [http-args (append (assq #:addr conf) (assq #:port conf))])
+    (format #t "* Starting md-webserver ~s~%" http-args)
+    (run-server (gen-req-handler conf) 'http http-args)))
